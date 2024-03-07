7b81c35b69b3df2f60087b2580fc5c3119e9399f
LichtPUBLIC <daffaalyandra65@gmail.com>
2024-03-07 12:28:44 +0700

    Add project files.

---
+++ Praktikum1/Praktikum1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int a[20];              // Deklarasi array a dengan ukuran 20
+int n;                  // Deklarasi variable n untuk meyimpan banyaknya elemen pada array
+
